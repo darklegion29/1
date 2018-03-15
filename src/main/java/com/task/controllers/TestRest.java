3e78e864fa2ea558251103c8eda2b7e401bcb98c
dark <darklegion29@gmail.com>
2018-03-15 18:34:05 +0200

    Base ready for view

--- src/main/java/com/task/controllers/TestRest.java
+++ src/main/java/com/task/controllers/TestRest.java
@@ -2,12 +2,15 @@ package com.task.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("test")
+//@RequestMapping("test")
 public class TestRest {
     private static final Logger logger = LoggerFactory.getLogger(TestRest.class);
 
@@ -19,5 +22,23 @@ public class TestRest {
 
         model.setViewName("log2");
         return model;
+    }
+
+    @RequestMapping("/u")
+    public ModelAndView u()
+    {
+        ModelAndView model = new ModelAndView();
+
+        model.setViewName("upload");
+        return model;
+    }
+
+    @RequestMapping("/f")
+    public String listUploadedFiles() throws IOException
+    {
+
+
+
+        return "uploadForm";
     }
 }
---
+++ src/main/java/com/task/libs/Day.java
@@ -0,0 +1,13 @@
+package com.task.libs;
+
+
+public enum Day
+{
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+}
